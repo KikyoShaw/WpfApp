b6316b9f21a39fcb21b54df40e4a062ca881aefb
shaw <1939484341@qq.com>
2022-09-19 15:36:12 +0800

    Turntable

---
+++ WpfShared/Converts/IndexToColor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfShared.Converts
+{
+    public class IndexToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var num = System.Convert.ToInt32(value);
+            var brush = Application.Current.Resources["CircularSingularSolidColorBrush"] as SolidColorBrush;
+            if (num % 2 == 1)
+                brush = Application.Current.Resources["CircularDualSolidColorBrush"] as SolidColorBrush;
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DrawPrizeIndexToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var num = System.Convert.ToInt32(value);
+            var brush = Application.Current.Resources["DrawPrizeSingularSolidColorBrush"] as SolidColorBrush;
+            if (num % 2 == 1)
+                brush = Application.Current.Resources["DrawPrizeDualSolidColorBrush"] as SolidColorBrush;
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfTurntable/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTurntable
+{
+    public class MenuItemModel
+    {
+        public double Angle { get; set; }
+        public string Title { get; set; }
+        public Brush FillColor { get; set; }
+        public ImageSource IconImage { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IEnumerable MenuArray
+        {
+            get => (IEnumerable)GetValue(MenuArrayProperty);
+            set => SetValue(MenuArrayProperty, value);
+        }
+
+        public static readonly DependencyProperty MenuArrayProperty =
+            DependencyProperty.Register("MenuArray", typeof(IEnumerable), typeof(MainWindow), 
+                new PropertyMetadata(null));
+
+
+        public List<int> ListAngle
+        {
+            get => (List<int>)GetValue(ListAngleProperty);
+            set => SetValue(ListAngleProperty, value);
+        }
+
+        public static readonly DependencyProperty ListAngleProperty =
+            DependencyProperty.Register("ListAngle", typeof(List<int>), typeof(MainWindow), 
+                new PropertyMetadata(null));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListAngle = new List<int>();
+            var menuItemModels = new List<MenuItemModel>();
+            var angle = 0;
+            var anglePrize = 2000;
+            for (int i = 0; i <= 7; i++)
+            {
+                var prizeTitle = i == 0 ? "谢谢参与" : $"{i}等奖";
+                angle += 45;
+                anglePrize += 45;
+                ListAngle.Add(anglePrize);
+                menuItemModels.Add(new MenuItemModel { Angle = angle, Title = prizeTitle });
+            }
+
+            MenuArray = menuItemModels;
+        }
+    }
+}
